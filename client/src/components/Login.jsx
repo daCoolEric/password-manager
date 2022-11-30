0b8c78adb00c0c7739ba132472a3d9d05c3a2db0
Dacooleric <dacooleric@gmail.com>
2022-11-30 06:15:29 -0800

    login designs

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,167 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import bgImg from '../images/loginpath.png';
+import emailImg from '../images/email.png';
+import padlockImg from '../images/padlock.png';
+import userImg from '../images/user.png';
+
+const Wrapper = styled.div`
+  // outline: 2px solid red;
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bgImg});
+  background-size: 100% 100vh;
+
+`
+const TextContainer = styled.div`
+  // outline: 2px solid blue;
+  height: 25vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  color: white;
+  font-size: 40px;
+`
+
+const LoginContainer = styled.div`
+  // outline: 2px solid red;
+  height: 75vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const LoginContent = styled.div`
+  // outline: 2px solid red;
+  width: 95%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const PersonalInfo = styled.div`
+  // outline: 2px solid blue;
+  width: 100%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding-top: 100px;
+`
+const EmailContainer = styled.div`
+  // outline: 2px solid red;
+  width: 95%;
+  height: 45px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #6a3cf7;
+`
+const IconBox = styled.div`
+  // outline: 2px solid red;
+  width: 10%;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const InputContainer = styled.input`
+  // outline: 2px solid red;
+  height: 80%;
+  width: 88%;
+  font-size: 25px;
+  color: #6a3cf7;
+  border-style: none;
+  
+`
+const PasswordContainer = styled.div`
+  // outline: 2px solid red;
+  width: 95%;
+  height: 45px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #6a3cf7;
+`
+
+const ForgotPasswordContainer = styled.div`
+  // outline: 2px solid red;
+  width: 95%;
+  height: 45px;
+  display: flex;
+  font-size: 20px;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const SubmitSection = styled.div`
+  // outline: 2px solid red;
+  width: 100%;
+  height: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+const LoginButton = styled.button`
+  // outline: 2px solid red;
+  width: 95%;
+  height: 50px;
+  border-radius: 25px;
+  border-style: none;
+  background-color: #6a3cf7;
+  color: #fff;
+  font-size: 27px;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const InfoSection = styled.div`
+  // outline: 2px solid red;
+  width: 100%;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+
 
 function Login() {
   return (
-    <>
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="1242.000000pt" height="2208.000000pt" viewBox="0 0 1242.000000 2208.000000" preserveAspectRatio="xMidYMid meet">
+    <Wrapper>
+      <TextContainer>Hello, Sign In</TextContainer>
+      <LoginContainer>
+        <LoginContent>
+          <PersonalInfo>
+            <EmailContainer>
+              <IconBox><img src={emailImg} alt="email icon" style={{ width: "100%" }} /></IconBox>
+              <InputContainer placeholder='Email'/>
+            </EmailContainer>
+            <PasswordContainer>
+            <IconBox><img src={padlockImg} alt="email icon" style={{ width: "100%" }} /></IconBox>
+              <InputContainer placeholder='Password'/>
+            </PasswordContainer>
+            <ForgotPasswordContainer>
+              Forgot Password?
+            </ForgotPasswordContainer>
+          </PersonalInfo>
+          <SubmitSection>
+            <LoginButton>Sign In</LoginButton>
+            <InfoSection>Don't have an account? <span style={{marginLeft: "10px", textDecoration: "none"}}> <Link to="/register" style={{textDecoration: "none"}}>Sign Up</Link></span></InfoSection>
+          </SubmitSection>
+        </LoginContent>
+      </LoginContainer>
 
-          <g transform="translate(0.000000,2208.000000) scale(0.100000,-0.100000)" fill="#fff" stroke="none">
-          <path d="M3003 16940 c-542 -37 -1081 -226 -1527 -538 -330 -232 -640 -564 -845 -908 -170 -285 -223 -419 -447 -1151 l-184 -602 0 -6870 0 -6871 6210 0 6210 0 0 7130 0 7129 -127 40 c-565 176 -8299 2539 -8372 2557 -275 70 -633 103 -918 84z"/>
-          </g>
-      </svg>
-    </>
+      
+      
+    </Wrapper>
   )
 }
 
